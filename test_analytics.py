9bd65deb357c356abdffc8b725f31e9c5ffcb03e
mohaubai <mubaid675@gmail.com>
2026-04-27 16:57:03 -0400

    Fixed test case code

--- test_analytics.py
+++ test_analytics.py
@@ -6,4 +6,4 @@ client = TestClient(app)
 def test_analytics_endpoint():
     response = client.get("/analytics")
     assert response.status_code == 200
-    assert isinstance(response.json()[0]['year'], int)
+    assert isinstance(response.json()['data'][0]['year'], int)
